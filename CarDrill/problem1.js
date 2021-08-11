8e82e37682cb7cab64bfdf503230afcbd3c00909
gopal-s-pal <gopal.pal.17@mountblue.tech>
2021-08-11 15:00:04 +0530

    changes commit

--- CarDrill/problem1.js
+++ CarDrill/problem1.js
@@ -3,10 +3,11 @@ module.exports = {
     {
         for (var i= 0; i< data.length; i++)
         {
-            if (data[i].id == 33)
+            if (data[i].id == 300003)
             {
                 return "Car " + data[i].id + " is a " + data[i].car_year+" "+ data[i].car_make+ " "+data[i].car_model;
             } 
         }
+        return []
     }
     }
